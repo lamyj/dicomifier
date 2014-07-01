690dc353c6d21e4ef159ad574b32d1a7185769e2
Romain Lahaxe <rom.lahaxe@gmail.com>
2014-07-01 11:41:22 +0200

    tests/code/factory/core: add unit test for RuleCreator

---
+++ tests/code/factory/core/RuleCreator.cpp
@@ -0,0 +1,49 @@
+/*************************************************************************
+ * Dicomifier - Copyright (C) Universite de Strasbourg
+ * Distributed under the terms of the CeCILL-B license, as published by
+ * the CEA-CNRS-INRIA. Refer to the LICENSE file or to
+ * http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+ * for details.
+ ************************************************************************/
+
+#define BOOST_TEST_MODULE ModuleRuleCreator
+#include <boost/foreach.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/test/unit_test.hpp>
+
+#include "core/Object.h"
+#include "core/Rule.h"
+#include "factory/core/RuleCreator.h"
+
+struct TestData
+{
+    boost::property_tree::ptree ptr;
+ 
+    TestData()
+    {
+        boost::property_tree::ptree rulenode;
+        boost::property_tree::ptree emptynode;
+        rulenode.add_child("Condition", emptynode);
+        rulenode.add_child("Actions", emptynode);
+        ptr.add_child("Rule", rulenode);
+    }
+ 
+    ~TestData()
+    {
+    }
+};
+
+BOOST_FIXTURE_TEST_CASE(Creation, TestData)
+{
+    auto testrule = dicomifier::factory::RuleCreator::New();
+    
+    BOOST_FOREACH(boost::property_tree::ptree::value_type &v, ptr)
+    {
+        dicomifier::Object::Pointer object = testrule->Create(v);
+        
+        dicomifier::Rule::Pointer rule = 
+                std::dynamic_pointer_cast<dicomifier::Rule>(object);
+        
+        BOOST_CHECK_EQUAL(rule != NULL, true);
+    }
+}
