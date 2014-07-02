36874b1d0e6da9ca4b3fa6c17c9f7fc5ac371d2c
Romain Lahaxe <rom.lahaxe@gmail.com>
2014-07-02 17:09:30 +0200

    tests/code/factory: update test files

--- tests/code/factory/dicom/actions/DeleteElementCreator.cpp
+++ tests/code/factory/dicom/actions/DeleteElementCreator.cpp
@@ -10,6 +10,7 @@
 #include <boost/foreach.hpp>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/test/unit_test.hpp>
+#include <boost/property_tree/xml_parser.hpp>
 
 #include "core/Object.h"
 #include "dicom/actions/DeleteElement.h"
@@ -21,13 +22,24 @@ struct TestData
  
     TestData()
     {
+        // Create Test file
+        DcmDataset* dataset = new DcmDataset();
+        OFString name("John");
+        dataset->putAndInsertOFStringArray(DCM_PatientName, name, true);
+        dataset->saveFile("./tempfile", EXS_LittleEndianExplicit);
+        delete dataset;
+        
+        // Create XML tree
         boost::property_tree::ptree emptynode;
-        emptynode.put("<xmlattr>.tag", "0010,0010");
+        emptynode.put("tag", "0010,0010");
+        emptynode.put("dataset", "./tempfile");
         ptr.add_child("DeleteElement", emptynode);
     }
  
     ~TestData()
     {
+        // Delete Test file
+        remove("./tempfile");
     }
 };
 
--- tests/code/factory/dicom/actions/EmptyElementCreator.cpp
+++ tests/code/factory/dicom/actions/EmptyElementCreator.cpp
@@ -21,13 +21,24 @@ struct TestData
  
     TestData()
     {
+        // Create Test file
+        DcmDataset* dataset = new DcmDataset();
+        OFString name("John");
+        dataset->putAndInsertOFStringArray(DCM_PatientName, name, true);
+        dataset->saveFile("./tempfile", EXS_LittleEndianExplicit);
+        delete dataset;
+        
+        // Create XML tree
         boost::property_tree::ptree emptynode;
-        emptynode.put("<xmlattr>.tag", "0010,0010");
+        emptynode.put("tag", "0010,0010");
+        emptynode.put("dataset", "./tempfile");
         ptr.add_child("EmptyElement", emptynode);
     }
  
     ~TestData()
     {
+        // Delete Test file
+        remove("./tempfile");
     }
 };
 
