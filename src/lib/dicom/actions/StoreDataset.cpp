0670cf4298f416a2e16807198874b90b3ae6dee5
Romain Lahaxe <rom.lahaxe@gmail.com>
2014-07-23 18:01:51 +0200

    src/lib: implement StoreDataset

--- src/lib/dicom/actions/StoreDataset.cpp
+++ src/lib/dicom/actions/StoreDataset.cpp
@@ -6,6 +6,8 @@
  * for details.
  ************************************************************************/
 
+#include <dcmtk/dcmnet/scu.h>
+
 #include "core/DicomifierException.h"
 #include "StoreDataset.h"
 
@@ -18,7 +20,7 @@ namespace actions
 StoreDataset::StoreDataset():
     _dataset(NULL),
     _address(""),
-    _port(""),
+    _port(0),
     _AEremote(""),
     _AElocal("")
 {
@@ -26,7 +28,7 @@ StoreDataset::StoreDataset():
 }
 
 StoreDataset::StoreDataset(DcmDataset* dataset, std::string const & address,
-                           std::string const & port, std::string const & aeremote,
+                           Uint16 const & port, std::string const & aeremote,
                            std::string const & aelocal):
     _dataset(dataset),
     _address(address),
@@ -50,6 +52,64 @@ StoreDataset
     {
         throw DicomifierException("Dataset is NULL");
     }
+    
+    // Get abstract syntax from dataset
+    OFString abstractSyntax;
+    OFCondition result = this->_dataset->findAndGetOFStringArray(DCM_SOPClassUID, abstractSyntax, OFTrue);
+    if (result.bad())
+    {
+        throw DicomifierException("Unable to retrieve Abstract Syntax");
+    }
+    
+    DcmSCU myscu;
+    
+    // Set AE title
+    myscu.setAETitle(this->_AElocal.c_str());
+    myscu.setPeerHostName(this->_address.c_str());
+    myscu.setPeerPort(this->_port);
+    myscu.setPeerAETitle(this->_AEremote.c_str());
+    
+    // Set transfert syntax
+    OFList<OFString> ts;
+    ts.push_back(UID_LittleEndianExplicitTransferSyntax);
+    ts.push_back(UID_LittleEndianImplicitTransferSyntax);
+    ts.push_back(UID_BigEndianExplicitTransferSyntax);
+    
+    myscu.addPresentationContext(abstractSyntax, ts);
+    
+    // Initialize Network
+    result = myscu.initNetwork();
+    if (result.bad())
+    {
+        throw DicomifierException("Unable to Initialize network");
+    }
+    
+    // Negotiate association
+    result = myscu.negotiateAssociation();
+    if (result.bad())
+    {
+        throw DicomifierException("Unable to Negotiate association");
+    }
+    
+    // Find presentation contexts
+    T_ASC_PresentationContextID pcid = myscu.findPresentationContextID(abstractSyntax, "");
+    if (pcid == 0)
+    {
+        throw DicomifierException("No adequate Presentation Contexts");
+    }
+    
+    // Do Storage
+    Uint16 rspstatus;
+    DcmDataset* rspcommandset;
+    DcmDataset* rspstatusdetail;
+    result = myscu.sendSTORERequest(pcid, "", this->_dataset, rspcommandset, rspstatusdetail, rspstatus);
+    if (result.bad() || rspstatus != 0)
+    {
+        throw DicomifierException("Could not process C-Store");
+    }
+    
+    // Release association
+    myscu.closeAssociation(DCMSCU_RELEASE_ASSOCIATION);
 }
 
 } // namespace actions
