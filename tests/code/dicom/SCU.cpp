0b8f4979daa6d588ebc5ac04225e5f3df4215fa1
Julien Lamy <lamy@unistra.fr>
2014-07-30 14:38:33 +0200

    tests/code/dicom: move DcmQrSCP to separate file

--- tests/code/dicom/SCU.cpp
+++ tests/code/dicom/SCU.cpp
@@ -12,6 +12,8 @@
 #include "dicom/SCU.h"
 #include "core/DicomifierException.h"
 
+#include "DcmQrSCP.h"
+
 BOOST_AUTO_TEST_CASE(Default)
 {
     dicomifier::SCU scu;
@@ -136,85 +138,6 @@ BOOST_AUTO_TEST_CASE(AffectedSOPClass)
     scu.set_affected_sop_class("1.2.840.10008.5.1.4.1.1.4");
     BOOST_CHECK_EQUAL(scu.get_affected_sop_class(), "1.2.840.10008.5.1.4.1.1.4");
 }
-
-/**
- * Test fixture starting a dcmqrscp processes.
- * 
- * The process listens on port 11112 and has AE title "REMOTE". Only AE named
- * "LOCAL" may connect to it. Its config file and storage directory are
- * temporary and deleted in the destructor.
- */
-class DcmQrSCP
-{
-public:
-    
-    
-    DcmQrSCP()
-    {
-        char template_[] = "/tmp/dicomifier.XXXXXX";
-        mkdtemp(template_);
-        this->_directory = template_;
-        
-        std::string const config = this->_directory+"/config";
-            
-        this->_pid = fork();
-        if(this->_pid == 0)
-        {
-            std::ofstream stream(config);
-            stream << "HostTable BEGIN\n";
-            stream << "remote = (REMOTE, localhost, 11112)\n";
-            stream << "local = (LOCAL, localhost, 0)\n";
-            stream << "HostTable END \n";
-            stream << "\n";
-            stream << "AETable BEGIN\n";
-            stream << "REMOTE " << this->_directory << " RW (10, 1024mb) local\n";
-            stream << "AETable END\n";
-            stream.close();
-            
-            execlp("dcmqrscp", "dcmqrscp", "-c", config.c_str(), "11112", NULL);
-            // The exec will not return on success.
-            throw std::runtime_error("Could not run dcmqrscp");
-        }
-        else
-        {
-            int status = system("echoscu -aec REMOTE -aet LOCAL localhost 11112");
-            while(WEXITSTATUS(status)>0)
-            {
-                // Wait a bit to make sure the process is running
-                usleep(100000);
-                status = system("echoscu -aec REMOTE -aet LOCAL localhost 11112");
-            }
-            if(WEXITSTATUS(status) == -1)
-            {
-                throw std::runtime_error("Could not wait for dcmqrscp");
-            }
-        }
-    }
-    
-    ~DcmQrSCP()
-    {
-        if(this->_pid != 0)
-        {
-            // Make sure boost does not complain about killing the dcmqrscp
-            struct sigaction old_action, new_action;
-            new_action.sa_handler = SIG_IGN;
-            sigaction(SIGCHLD, &new_action, &old_action);
-            
-            // Kill the process, remove its data on disk
-            kill(this->_pid, SIGKILL);
-            std::string const command = "rm -rf "+this->_directory;
-            system(command.c_str());
-            
-            // Restore the original signal handler
-            sigaction(SIGCHLD, &old_action, NULL);
-        }
-    }
-    
-private:
-    std::string _directory;
-    std::string _config;
-    pid_t _pid;
-};
 
 BOOST_FIXTURE_TEST_CASE(ReAssociate, DcmQrSCP)
 {
