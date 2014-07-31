76db95ed21008326eeef7aa1a01181932e21aada
Julien Lamy <lamy@unistra.fr>
2014-07-31 11:18:00 +0200

    tests/code/dicom/DcmQrSCP: assume an external DICOM SCP as boost::test and process spawning is not robust

--- tests/code/dicom/DcmQrSCP.h
+++ tests/code/dicom/DcmQrSCP.h
@@ -9,92 +9,60 @@
 #ifndef _f515ed49_8c2b_4fc1_b7db_fdc3ec747861
 #define _f515ed49_8c2b_4fc1_b7db_fdc3ec747861
 
-#include <fstream>
-#include <signal.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <string>
-#include <unistd.h>
-#include <sys/types.h>
+
+#include <boost/lexical_cast.hpp>
 
 /**
- * Test fixture starting a dcmqrscp processes.
- * 
- * The process listens on port 11112 and has AE title "REMOTE". Only AE named
- * "LOCAL" may connect to it. Its config file and storage directory are
- * temporary and deleted in the destructor.
+ * Test fixture getting the environment variables required to connect to a DICOM
+ * query/retrieve SCP.
  */
 class DcmQrSCP
 {
 public:
     
+    std::string const data_directory;
+    std::string const calling_aet;
+    std::string const peer_host;
+    uint16_t const peer_port;
+    std::string const peer_aet;
     
     DcmQrSCP()
+    : data_directory(this->_get_environment_variable("DICOMIFIER_TEST_DATA")),
+      calling_aet(this->_get_environment_variable("DICOMIFIER_CALLING_AET")),
+      peer_host(this->_get_environment_variable("DICOMIFIER_PEER_HOST")),
+      peer_port(this->_get_environment_variable<uint16_t>("DICOMIFIER_PEER_PORT")),
+      peer_aet(this->_get_environment_variable("DICOMIFIER_PEER_AET"))
     {
-        char template_[] = "/tmp/dicomifier.XXXXXX";
-        mkdtemp(template_);
-        this->_directory = template_;
-        
-        std::string const config = this->_directory+"/config";
-            
-        this->_pid = fork();
-        if(this->_pid == 0)
-        {
-            std::ofstream stream(config);
-            stream << "HostTable BEGIN\n";
-            stream << "remote = (REMOTE, localhost, 11112)\n";
-            stream << "local = (LOCAL, localhost, 0)\n";
-            stream << "HostTable END \n";
-            stream << "\n";
-            stream << "AETable BEGIN\n";
-            stream << "REMOTE " << this->_directory << " RW (10, 1024mb) local\n";
-            stream << "AETable END\n";
-            stream.close();
-            
-            execlp("dcmqrscp", "dcmqrscp", "-c", config.c_str(), "11112", NULL);
-            // The exec will not return on success.
-            throw std::runtime_error("Could not run dcmqrscp");
-        }
-        else
-        {
-            int status = system("echoscu -aec REMOTE -aet LOCAL localhost 11112");
-            int count = 0;
-            while(WEXITSTATUS(status)>0 && count < 10)
-            {
-                // Wait a bit to make sure the process is running
-                usleep(100000);
-                status = system("echoscu -aec REMOTE -aet LOCAL localhost 11112");
-                ++count;
-            }
-            if(WEXITSTATUS(status) == -1 || count >= 10)
-            {
-                throw std::runtime_error("Could not wait for dcmqrscp");
-            }
-        }
+        // Nothing else
     }
     
     ~DcmQrSCP()
     {
-        if(this->_pid != 0)
-        {
-            // Make sure boost does not complain about killing the dcmqrscp
-            struct sigaction old_action, new_action;
-            new_action.sa_handler = SIG_IGN;
-            sigaction(SIGCHLD, &new_action, &old_action);
-            
-            // Kill the process, remove its data on disk
-            kill(this->_pid, SIGKILL);
-            std::string const command = "rm -rf "+this->_directory;
-            system(command.c_str());
-            
-            // Restore the original signal handler
-            sigaction(SIGCHLD, &old_action, NULL);
-        }
+        // Nothing to do.
     }
     
 private:
-    std::string _directory;
-    std::string _config;
-    pid_t _pid;
+    
+    std::string _get_environment_variable(std::string const & name) const
+    {
+        char* value = getenv(name.c_str());
+        if(value == NULL)
+        {
+            BOOST_FAIL(name + " is not defined");
+        }
+        return value;
+    }
+    
+    template<typename T>
+    T _get_environment_variable(std::string const & name) const
+    {
+        return boost::lexical_cast<T>(this->_get_environment_variable(name));
+    }
+    
+    
 };
 
 #endif // _f515ed49_8c2b_4fc1_b7db_fdc3ec747861
