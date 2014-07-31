55f9c1e292ad48ded3fc95c1e1b7d7c0779c9e0a
Julien Lamy <lamy@unistra.fr>
2014-07-31 12:49:06 +0200

    src/lib/dicom/actions/StoreDataset: add user identity handling

--- src/lib/dicom/actions/StoreDataset.h
+++ src/lib/dicom/actions/StoreDataset.h
@@ -13,6 +13,7 @@
 #include <dcmtk/dcmdata/dctk.h>     /* Covers most common dcmdata classes */
 
 #include "core/actions/Action.h"
+#include "dicom/SCU.h"
 
 namespace dicomifier
 {
@@ -28,11 +29,18 @@ public:
     typedef std::shared_ptr<Self const> ConstPointer;
     
     static Pointer New() { return Pointer(new Self()); }
-    static Pointer New(DcmDataset* dataset, std::string const & address,
-                       Uint16 const & port, std::string const & aeremote,
-                       std::string const & aelocal, std::string const & user,
-                       std::string const & password) 
-        { return Pointer(new Self(dataset, address, port, aeremote, aelocal, user, password)); }
+    static Pointer New(DcmDataset* dataset, 
+        std::string const & address, Uint16 const & port, 
+        std::string const & aeremote, std::string const & aelocal, 
+        UserIdentityType const & user_identity_type=UserIdentityType::None,
+        std::string const & user_identity_primary_field="",
+        std::string const & user_identity_secondary_field="") 
+        { 
+            return Pointer(new Self(
+                dataset, address, port, aeremote, aelocal, 
+                user_identity_type, 
+                user_identity_primary_field, user_identity_secondary_field)); 
+        }
     
     virtual ~StoreDataset();
 
@@ -51,11 +59,20 @@ public:
     std::string get_AElocal() const { return this->_AElocal; }
     void set_AElocal(std::string const & aelocal) { this->_AElocal = aelocal; }
     
-    std::string get_user() const { return this->_user; }
-    void set_user(std::string const & user) { this->_user = user; }
+    /// Return the user identity type. Defaults to None.
+    UserIdentityType get_user_identity_type() const;
+    /// Set the user identity type.
+    void set_user_identity_type(UserIdentityType type);
     
-    std::string get_password() const { return this->_password; }
-    void set_password(std::string const & pwd) { this->_password = pwd; }
+    /// Return the user identity primary field. Defaults to "".
+    std::string const & get_user_identity_primary_field() const;
+    /// Set the user identity primary field. 
+    void set_user_identity_primary_field(std::string const & value);
+    
+    /// Return the user identity secondary field. Defaults to "".
+    std::string const & get_user_identity_secondary_field() const;
+    /// Set the user identity secondary field.
+    void set_user_identity_secondary_field(std::string const & value);
 
     virtual void run() const;
     
@@ -63,10 +80,12 @@ public:
     
 protected:
     StoreDataset();
-    StoreDataset(DcmDataset* dataset, std::string const & address,
-                 Uint16 const & port, std::string const & aeremote,
-                 std::string const & aelocal, std::string const & user,
-                 std::string const & password);
+    StoreDataset(DcmDataset* dataset, 
+        std::string const & address, Uint16 const & port, 
+        std::string const & aeremote, std::string const & aelocal, 
+        UserIdentityType const & user_identity_type=UserIdentityType::None,
+        std::string const & user_identity_primary_field="",
+        std::string const & user_identity_secondary_field="");
 
 private:
     DcmDataset* _dataset;
@@ -74,8 +93,9 @@ private:
     Uint16 _port;
     std::string _AEremote;
     std::string _AElocal;
-    std::string _user;
-    std::string _password;
+    UserIdentityType _user_identity_type;
+    std::string _user_identity_primary_field;
+    std::string _user_identity_secondary_field;
 
     StoreDataset(Self const & other); // Purposely not implemented
     Self const & operator=(Self const & other); // Purposely not implemented
