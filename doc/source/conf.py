d112c8eceb7350da1ea156431997bff306fcea5c
Romain Lahaxe <rom.lahaxe@gmail.com>
2014-08-26 11:26:10 +0200

    doc: update documentation, add Internationalization

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -260,3 +260,8 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+# -- Options for Internationalization -------------------------------------
+
+locale_dirs = ['language/']   # path is example but recommended.
+gettext_compact = False     # optional.
