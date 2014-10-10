6b80e6ad8e8d382b27620df3b7b62a070cf55068
Romain Lahaxe <rom.lahaxe@gmail.com>
2014-10-10 10:42:07 +0200

    src/lib/core: modify index generator

--- tests/code/core/FrameIndexGenerator.cpp
+++ tests/code/core/FrameIndexGenerator.cpp
@@ -17,135 +17,177 @@
  */
 BOOST_AUTO_TEST_CASE(TEST_OK_01)
 {
-    auto generator = new dicomifier::FrameIndexGenerator({10, 2, 20});
+    auto generator = new dicomifier::FrameIndexGenerator({10, 2});
     BOOST_CHECK_EQUAL(generator != NULL, true);
     delete generator;
 }
 
 /*************************** TEST OK 02 *******************************/
 /**
- * Nominal test case: One value
+ * Nominal test case: Simple counter
  */
 BOOST_AUTO_TEST_CASE(TEST_OK_02)
 {
-    auto generator = new dicomifier::FrameIndexGenerator({20});
+    auto generator = new dicomifier::FrameIndexGenerator({}, 2);
+    
+    BOOST_CHECK_EQUAL(generator->done(), false);
+    BOOST_CHECK_EQUAL(generator->get_step(), 0);
+    generator->next();
+    
+    BOOST_CHECK_EQUAL(generator->done(), false);
+    BOOST_CHECK_EQUAL(generator->get_step(), 1);
+    generator->next();
     
     BOOST_CHECK_EQUAL(generator->done(), true);
-    
-    BOOST_CHECK_EQUAL(generator->next().size(), 1);
-    BOOST_CHECK_EQUAL(generator->next()[0], 20);
+    BOOST_CHECK_EQUAL(generator->get_step(), 2);
     
     delete generator;
 }
 
 /*************************** TEST OK 03 *******************************/
 /**
- * Nominal test case: Two value
+ * Nominal test case: Index Generator
  */
 BOOST_AUTO_TEST_CASE(TEST_OK_03)
 {
-    auto generator = new dicomifier::FrameIndexGenerator({2,2});
-    
+    auto generator = new dicomifier::FrameIndexGenerator({2});
+
     BOOST_CHECK_EQUAL(generator->done(), false);
-    
-    std::vector<int> values = generator->next();
-    BOOST_CHECK_EQUAL(values.size(), 2);
+    BOOST_CHECK_EQUAL(generator->get_step(), 0);
+    auto values = generator->get_index();
+    BOOST_CHECK_EQUAL(values.size(), 1);
     BOOST_CHECK_EQUAL(values[0], 0);
-    BOOST_CHECK_EQUAL(values[1], 2);
+    generator->next();
     
     BOOST_CHECK_EQUAL(generator->done(), false);
-    
-    values = generator->next();
-    BOOST_CHECK_EQUAL(values.size(), 2);
+    BOOST_CHECK_EQUAL(generator->get_step(), 1);
+    values = generator->get_index();
+    BOOST_CHECK_EQUAL(values.size(), 1);
     BOOST_CHECK_EQUAL(values[0], 1);
-    BOOST_CHECK_EQUAL(values[1], 2);
+    generator->next();
     
     BOOST_CHECK_EQUAL(generator->done(), true);
+    BOOST_CHECK_EQUAL(generator->get_step(), 2);
+    values = generator->get_index();
+    BOOST_CHECK_EQUAL(values.size(), 1);
+    BOOST_CHECK_EQUAL(values[0], 0);
     
     delete generator;
 }
 
 /*************************** TEST OK 04 *******************************/
 /**
- * Nominal test case: Multiple (> 2) value
+ * Nominal test case: Index Generator
  */
 BOOST_AUTO_TEST_CASE(TEST_OK_04)
 {
-    auto generator = new dicomifier::FrameIndexGenerator({2,2,2,8});
+    auto generator = new dicomifier::FrameIndexGenerator({159}, 2);
     
     BOOST_CHECK_EQUAL(generator->done(), false);
-    
-    std::vector<int> values = generator->next();
-    BOOST_CHECK_EQUAL(values.size(), 4);
+    BOOST_CHECK_EQUAL(generator->get_step(), 0);
+    auto values = generator->get_index();
+    BOOST_CHECK_EQUAL(values.size(), 1);
     BOOST_CHECK_EQUAL(values[0], 0);
-    BOOST_CHECK_EQUAL(values[1], 0);
-    BOOST_CHECK_EQUAL(values[2], 0);
-    BOOST_CHECK_EQUAL(values[3], 8);
+    generator->next();
     
     BOOST_CHECK_EQUAL(generator->done(), false);
-    
-    values = generator->next();
-    BOOST_CHECK_EQUAL(values.size(), 4);
-    BOOST_CHECK_EQUAL(values[0], 0);
-    BOOST_CHECK_EQUAL(values[1], 0);
-    BOOST_CHECK_EQUAL(values[2], 1);
-    BOOST_CHECK_EQUAL(values[3], 8);
-    
-    BOOST_CHECK_EQUAL(generator->done(), false);
-    
-    values = generator->next();
-    BOOST_CHECK_EQUAL(values.size(), 4);
-    BOOST_CHECK_EQUAL(values[0], 0);
-    BOOST_CHECK_EQUAL(values[1], 1);
-    BOOST_CHECK_EQUAL(values[2], 0);
-    BOOST_CHECK_EQUAL(values[3], 8);
-    
-    BOOST_CHECK_EQUAL(generator->done(), false);
-    
-    values = generator->next();
-    BOOST_CHECK_EQUAL(values.size(), 4);
-    BOOST_CHECK_EQUAL(values[0], 0);
-    BOOST_CHECK_EQUAL(values[1], 1);
-    BOOST_CHECK_EQUAL(values[2], 1);
-    BOOST_CHECK_EQUAL(values[3], 8);
-    
-    BOOST_CHECK_EQUAL(generator->done(), false);
-    
-    values = generator->next();
-    BOOST_CHECK_EQUAL(values.size(), 4);
+    BOOST_CHECK_EQUAL(generator->get_step(), 1);
+    values = generator->get_index();
+    BOOST_CHECK_EQUAL(values.size(), 1);
     BOOST_CHECK_EQUAL(values[0], 1);
-    BOOST_CHECK_EQUAL(values[1], 0);
-    BOOST_CHECK_EQUAL(values[2], 0);
-    BOOST_CHECK_EQUAL(values[3], 8);
-    
-    BOOST_CHECK_EQUAL(generator->done(), false);
-    
-    values = generator->next();
-    BOOST_CHECK_EQUAL(values.size(), 4);
-    BOOST_CHECK_EQUAL(values[0], 1);
-    BOOST_CHECK_EQUAL(values[1], 0);
-    BOOST_CHECK_EQUAL(values[2], 1);
-    BOOST_CHECK_EQUAL(values[3], 8);
-    
-    BOOST_CHECK_EQUAL(generator->done(), false);
-    
-    values = generator->next();
-    BOOST_CHECK_EQUAL(values.size(), 4);
-    BOOST_CHECK_EQUAL(values[0], 1);
-    BOOST_CHECK_EQUAL(values[1], 1);
-    BOOST_CHECK_EQUAL(values[2], 0);
-    BOOST_CHECK_EQUAL(values[3], 8);
-    
-    BOOST_CHECK_EQUAL(generator->done(), false);
-    
-    values = generator->next();
-    BOOST_CHECK_EQUAL(values.size(), 4);
-    BOOST_CHECK_EQUAL(values[0], 1);
-    BOOST_CHECK_EQUAL(values[1], 1);
-    BOOST_CHECK_EQUAL(values[2], 1);
-    BOOST_CHECK_EQUAL(values[3], 8);
+    generator->next();
     
     BOOST_CHECK_EQUAL(generator->done(), true);
+    BOOST_CHECK_EQUAL(generator->get_step(), 2);
+    values = generator->get_index();
+    BOOST_CHECK_EQUAL(values.size(), 1);
+    BOOST_CHECK_EQUAL(values[0], 2);
+    
+    delete generator;
+}
+
+/*************************** TEST OK 05 *******************************/
+/**
+ * Nominal test case: Multiple (> 2) value
+ */
+BOOST_AUTO_TEST_CASE(TEST_OK_05)
+{
+    auto generator = new dicomifier::FrameIndexGenerator({2,2,2});
+    
+    BOOST_CHECK_EQUAL(generator->done(), false);
+    BOOST_CHECK_EQUAL(generator->get_step(), 0);
+    auto values = generator->get_index();
+    BOOST_CHECK_EQUAL(values.size(), 3);
+    BOOST_CHECK_EQUAL(values[0], 0);
+    BOOST_CHECK_EQUAL(values[1], 0);
+    BOOST_CHECK_EQUAL(values[2], 0);
+    generator->next();
+    
+    BOOST_CHECK_EQUAL(generator->done(), false);
+    BOOST_CHECK_EQUAL(generator->get_step(), 1);
+    values = generator->get_index();
+    BOOST_CHECK_EQUAL(values.size(), 3);
+    BOOST_CHECK_EQUAL(values[0], 0);
+    BOOST_CHECK_EQUAL(values[1], 0);
+    BOOST_CHECK_EQUAL(values[2], 1);
+    generator->next();
+    
+    BOOST_CHECK_EQUAL(generator->done(), false);
+    BOOST_CHECK_EQUAL(generator->get_step(), 2);
+    values = generator->get_index();
+    BOOST_CHECK_EQUAL(values.size(), 3);
+    BOOST_CHECK_EQUAL(values[0], 0);
+    BOOST_CHECK_EQUAL(values[1], 1);
+    BOOST_CHECK_EQUAL(values[2], 0);
+    generator->next();
+    
+    BOOST_CHECK_EQUAL(generator->done(), false);
+    BOOST_CHECK_EQUAL(generator->get_step(), 3);
+    values = generator->get_index();
+    BOOST_CHECK_EQUAL(values.size(), 3);
+    BOOST_CHECK_EQUAL(values[0], 0);
+    BOOST_CHECK_EQUAL(values[1], 1);
+    BOOST_CHECK_EQUAL(values[2], 1);
+    generator->next();
+    
+    BOOST_CHECK_EQUAL(generator->done(), false);
+    BOOST_CHECK_EQUAL(generator->get_step(), 4);
+    values = generator->get_index();
+    BOOST_CHECK_EQUAL(values.size(), 3);
+    BOOST_CHECK_EQUAL(values[0], 1);
+    BOOST_CHECK_EQUAL(values[1], 0);
+    BOOST_CHECK_EQUAL(values[2], 0);
+    generator->next();
+    
+    BOOST_CHECK_EQUAL(generator->done(), false);
+    BOOST_CHECK_EQUAL(generator->get_step(), 5);
+    values = generator->get_index();
+    BOOST_CHECK_EQUAL(values.size(), 3);
+    BOOST_CHECK_EQUAL(values[0], 1);
+    BOOST_CHECK_EQUAL(values[1], 0);
+    BOOST_CHECK_EQUAL(values[2], 1);
+    generator->next();
+    
+    BOOST_CHECK_EQUAL(generator->done(), false);
+    BOOST_CHECK_EQUAL(generator->get_step(), 6);
+    values = generator->get_index();
+    BOOST_CHECK_EQUAL(values.size(), 3);
+    BOOST_CHECK_EQUAL(values[0], 1);
+    BOOST_CHECK_EQUAL(values[1], 1);
+    BOOST_CHECK_EQUAL(values[2], 0);
+    generator->next();
+    
+    BOOST_CHECK_EQUAL(generator->done(), false);
+    BOOST_CHECK_EQUAL(generator->get_step(), 7);
+    values = generator->get_index();
+    BOOST_CHECK_EQUAL(values.size(), 3);
+    BOOST_CHECK_EQUAL(values[0], 1);
+    BOOST_CHECK_EQUAL(values[1], 1);
+    BOOST_CHECK_EQUAL(values[2], 1);
+    generator->next();
+    
+    BOOST_CHECK_EQUAL(generator->done(), true);
+    BOOST_CHECK_EQUAL(generator->get_step(), 8);
     
     delete generator;
 }
