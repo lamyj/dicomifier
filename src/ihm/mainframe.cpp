22452911abb51bf4d25b46fea988f9c428a10115
Romain Lahaxe <rom.lahaxe@gmail.com>
2014-10-29 17:15:43 +0100

    src/ihm: modify main window and add subjects widget

--- src/ihm/mainframe.cpp
+++ src/ihm/mainframe.cpp
@@ -1,11 +1,19 @@
+#include "SubjectsFrame.h"
 #include "mainframe.h"
 #include "ui_mainframe.h"
+
+#include <QGridLayout>
 
 MainFrame::MainFrame(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainFrame)
 {
     ui->setupUi(this);
+
+    /*QGridLayout m_MainLayout;
+    m_MainLayout.setMargin(0);  // No space between window's element and the border
+    m_MainLayout.setSpacing(0); // No space between window's element
+    setLayout(&m_MainLayout);*/
 }
 
 MainFrame::~MainFrame()
@@ -15,12 +23,6 @@ MainFrame::~MainFrame()
 
 void MainFrame::Initialize()
 {
-    // Remove title bar
-    this->setWindowFlags(Qt::CustomizeWindowHint);
-
     // Window Position and Size
     this->setGeometry(0, 0, 1024, 768);
-
-    // Connect Signals <-> Slots
-    connect(ui->quitButton, SIGNAL(clicked()), this, SLOT(close()));
 }
