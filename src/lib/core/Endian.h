e067783719240fecd3b97bbb3c64e1e7b88ad613
Julien Lamy <lamy@unistra.fr>
2015-06-22 18:29:06 +0200

    Fix declaration of endianness functions

--- src/lib/core/Endian.h
+++ src/lib/core/Endian.h
@@ -20,26 +20,12 @@ namespace endian
 /**
  * @brief Test if the current byte order is big endian.
  */
-bool is_big_endian()
-{
-    union Endianness
-    {
-        uint16_t i;
-        char c[2];
-    };
-    Endianness endianness;
-    endianness.i = 0x0102;
-
-    return (endianness.c[0] == 1);
-}
+bool is_big_endian();
 
 /**
  * @brief Test if the current byte order is little endian.
  */
-bool is_little_endian()
-{
-    return !is_big_endian();
-}
+bool is_little_endian();
 
 /**
  * @brief Swap the endianness of given object in place.
