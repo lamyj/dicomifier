9cdff8da257473736fdb36a9f5d748912d95ccea
Lahaxe <lahaxe@unistra.fr>
2015-07-30 14:39:53 +0200

    src/lib/converters: fix error with software and software version

--- src/lib/bruker/converters/equipment.h
+++ src/lib/bruker/converters/equipment.h
@@ -11,6 +11,9 @@
 
 #include <memory>
 
+#include <boost/algorithm/string.hpp>
+#include <boost/algorithm/string/split.hpp>
+
 #include <dcmtkpp/registry.h>
 #include <dcmtkpp/Tag.h>
 
@@ -36,21 +39,37 @@ std::map<dcmtkpp::Tag, converter_base::pointer> const general_equipment = {
     { dcmtkpp::registry::SoftwareVersions,
         std::make_shared<generic_converter>(
           [](Dataset const & dataset, dicomifier::FrameIndexGenerator const & , dcmtkpp::Value & value) {
+              // VisuCreator - Creator application of this parameter set or data set. Two entries
+              // are allowed separated by a semicolon.
               if(dataset.has_field("VisuCreator"))
               {
-                auto const & creator = dataset.get_field("VisuCreator");
-                for(int i=0; i<creator.get_size(); ++i)
-                  {
-                      value.as_strings().push_back(creator.get_string(i));
-                  }
-              }
-              if(dataset.has_field("VisuCreatorVersion"))
-              {
-                  auto const & version = dataset.get_field("VisuCreatorVersion");
-                  for(int i=0; i<version.get_size(); ++i)
-                  {
-                      value.as_strings().push_back(version.get_string(i));
-                  }
+                auto const creator = dataset.get_field("VisuCreator").get_string(0);
+
+                std::string version = "";
+                // VisuCreatorVersion - Creator application version of this parameter set or data
+                // set. They must match with the entries in VisuCreator and, therefore, can also
+                // have two entries separated by semicolons.
+                if(dataset.has_field("VisuCreatorVersion"))
+                {
+                  version = dataset.get_field("VisuCreatorVersion").get_string(0);
+                }
+
+                std::vector<std::string> creator_values;
+                boost::split(creator_values, creator, boost::is_any_of(";"));
+
+                std::vector<std::string> version_values;
+                boost::split(version_values, version, boost::is_any_of(";"));
+
+                for(int i=0; i<creator_values.size(); ++i)
+                {
+                    std::stringstream creator_version;
+                    creator_version << creator_values[i];
+                    if (version_values.size() > i)
+                    {
+                        creator_version << " " << version_values[i];
+                    }
+                    value.as_strings().push_back(creator_version.str());
+                }
               }
           }) },
 };
