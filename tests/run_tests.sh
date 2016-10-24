218bccdd44d23f911cf2f08b085176e389ee9607
Julien Lamy <lamy@unistra.fr>
2016-10-24 14:44:46 +0200

    Fix PYTHONPATH

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -5,6 +5,7 @@ ctest --no-compress-output -T Test $@ || true
 PYTHONPATH=../src/python
 PYTHONPATH=${PYTHONPATH}:../build/src/python/dicomifier
 PYTHONPATH=${PYTHONPATH}:../build/src/python/dicomifier/bruker
+PYTHONPATH=${PYTHONPATH}:../build/src/python/dicomifier/nifti
 export PYTHONPATH
 
 nosetests-2.7 ../tests/python/dicomifier
