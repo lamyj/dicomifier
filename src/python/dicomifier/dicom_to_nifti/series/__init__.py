24d534a03d98da616e1e424c077f03e8962eb4c6
Julien Lamy <lamy@unistra.fr>
2020-03-24 17:08:51 +0100

    Add license header

--- src/python/dicomifier/dicom_to_nifti/series/__init__.py
+++ src/python/dicomifier/dicom_to_nifti/series/__init__.py
@@ -1,3 +1,11 @@
+#########################################################################
+# Dicomifier - Copyright (C) Universite de Strasbourg
+# Distributed under the terms of the CeCILL-B license, as published by
+# the CEA-CNRS-INRIA. Refer to the LICENSE file or to
+# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+# for details.
+#########################################################################
+
 from .default_series_finder import DefaultSeriesFinder
 from .siemens_xa_classic_2d_series_finder import SiemensXAClassic2DSeriesFinder
 
