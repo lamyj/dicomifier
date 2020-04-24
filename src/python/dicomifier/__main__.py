88467f21f5a65e1709e6df7566aceee3ec50ad72
Julien Lamy <lamy@unistra.fr>
2020-04-24 17:31:36 +0200

    Add to-dicom command

---
+++ src/python/dicomifier/__main__.py
@@ -0,0 +1,52 @@
+#########################################################################
+# Dicomifier - Copyright (C) Universite de Strasbourg
+# Distributed under the terms of the CeCILL-B license, as published by
+# the CEA-CNRS-INRIA. Refer to the LICENSE file or to
+# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+# for details.
+#########################################################################
+
+import argparse
+import logging
+import sys
+
+from . import commands
+
+def main():
+    parser = argparse.ArgumentParser(description="Dicomifier")
+    
+    parser.add_argument(
+        "--verbosity", "-v",
+        choices=["warning", "info", "debug"], default="warning")
+    
+    subparsers = parser.add_subparsers(help="Available commands")
+    command_parsers = {}
+    for name in dir(commands):
+        if name.startswith("__"):
+            continue
+        command = getattr(commands, name)
+        subparser = command.setup(subparsers)
+        subparser.add_argument(
+            "--verbosity", "-v",
+            choices=["warning", "info", "debug"], default="warning")
+        subparser.set_defaults(action=command.action)
+        command_parsers[command.action] = subparser
+    
+    arguments = vars(parser.parse_args())
+    
+    verbosity = arguments.pop("verbosity")
+    logging.basicConfig(
+        level=verbosity.upper(), 
+        format="%(levelname)s - %(name)s: %(message)s")
+    
+    action = arguments.pop("action")
+    try:
+        action(**arguments)
+    except Exception as e:
+        if verbosity == "debug":
+            raise
+        else:
+            command_parsers[action].error(e)
+
+if __name__ == "__main__":
+    sys.exit(main())
