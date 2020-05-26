6983a9889220788636e0d6961d7620a7577374d1
Julien Lamy <lamy@unistra.fr>
2020-05-26 12:24:54 +0200

    Add doc to wrappers

--- src/python/dicomifier/bruker/Dataset.cpp
+++ src/python/dicomifier/bruker/Dataset.cpp
@@ -17,17 +17,28 @@ void wrap_Dataset(pybind11::module & m)
     using namespace pybind11;
     using namespace dicomifier::bruker;
     
-    class_<Dataset>(m, "Dataset")
-        .def(init<>())
-        .def("load", &Dataset::load)
-        .def("has_field", &Dataset::has_field)
+    class_<Dataset>(
+            m, "Dataset", R"doc(
+                A Bruker dataset. This objects implements part of the mapping 
+                protocol: ``"foo" in d``, ``d["foo"]`` and ``for x in d``
+                are valid constructs.)doc")
+        .def(init<>(), "init docstring")
         .def(
-            "get_field", &Dataset::get_field, 
-            return_value_policy::reference_internal)
+            "load", &Dataset::load, arg("path"),
+            "Load dataset from file, update any existing field.")
+        .def(
+            "has_field", &Dataset::has_field, arg("name"),
+            "Test if dataset contains a given field.")
+        .def(
+            "get_field", &Dataset::get_field, arg("name"),
+            return_value_policy::reference_internal,
+            "Return the field, throw an exception if field is missing.")
         // TODO? set_field
         .def(
             "get_used_files", &Dataset::get_used_files, 
-            return_value_policy::reference_internal)
+            return_value_policy::reference_internal, R"doc(
+                Return a set of files used to create the dataset 
+                (except the PixelData file))doc")
         .def("__contains__", &Dataset::has_field)
         .def(
             "__getitem__", &Dataset::get_field, 
@@ -40,6 +51,6 @@ void wrap_Dataset(pybind11::module & m)
         .def(
             "items", 
             [](Dataset const & d) { return make_iterator(d.begin(), d.end()); },
-            keep_alive<0, 1>())
+            keep_alive<0, 1>(), "Iterate on fields")
     ;
 }
