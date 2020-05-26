f86d04d6ca6616f43fa5bf7272e3a0798ccb1080
Julien Lamy <lamy@unistra.fr>
2020-05-26 12:57:52 +0200

    Fix conversion to bvecs/bvals

--- src/python/dicomifier/nifti/diffusion.py
+++ src/python/dicomifier/nifti/diffusion.py
@@ -58,16 +58,16 @@ def to_mrtrix(scheme, fd):
         b_value /= 1e6
         print(*direction, b_value, file=fd)
 
-def to_fsl(scheme, image, bvecs_fd, bvals_fd):
-    """ Save a diffusion scheme in FSL bvecs+bvals format. A reference nibabel
-        image is required as the bvecs are store in image coordinates, not in 
-        patient coordinates.
+def to_fsl(scheme, transform, bvecs_fd, bvals_fd):
+    """ Save a diffusion scheme in FSL bvecs+bvals format. A reference 
+        transform is required as the bvecs are store in image coordinates, not 
+        in patient coordinates. This transform must correspond to an 
+        image-to-patient transform, e.g. what is stored in the *affine* member
+        of nibabel images.
     """
     
     # https://fsl.fmrib.ox.ac.uk/fsl/fslwiki/FDT/UserGuide#Diffusion_data_in_FSL
     # https://fsl.fmrib.ox.ac.uk/fsl/fslwiki/FDT/FAQ#What_conventions_do_the_bvecs_use.3F
-    
-    transform = image.affine[:3,:3]
     
     directions = numpy.array([direction for b_value, direction in scheme])
     # Convert from patient coordinates to image coordinates
@@ -77,6 +77,8 @@ def to_fsl(scheme, image, bvecs_fd, bvals_fd):
     
     if numpy.linalg.det(transform)>0:
         bvecs[:,0] *= -1
+    
+    bvecs[bvecs == numpy.NZERO] = 0
     
     # Re-normalize (not required by FSL)
     norm = numpy.maximum(1e-20, numpy.linalg.norm(bvecs, axis=1))
