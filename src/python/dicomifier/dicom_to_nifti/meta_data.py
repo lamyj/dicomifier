b3d52b44ef0e1f4d27366bd482f1755e0df39e0a
Julien Lamy <lamy@unistra.fr>
2021-01-15 08:51:05 +0100

    Remove debug print

--- src/python/dicomifier/dicom_to_nifti/meta_data.py
+++ src/python/dicomifier/dicom_to_nifti/meta_data.py
@@ -85,7 +85,6 @@ def get_meta_data(stack, cache=None):
         try:
             sop_instance_uid = data_set[odil.registry.SOPInstanceUID][0]
         except Exception as e:
-            print(odil.as_xml(data_set, True))
             raise
         # Fetch non-frame-specific elements from cache
         for tag, element in cache[sop_instance_uid].items():
