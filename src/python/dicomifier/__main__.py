a43ab7f896bfde73a38a52d8968c1f92e7da0050
Julien Lamy <lamy@unistra.fr>
2024-03-11 10:51:44 +0100

    Bump version: 2.4.0 → 2.5.0

--- src/python/dicomifier/__main__.py
+++ src/python/dicomifier/__main__.py
@@ -15,7 +15,7 @@ from . import commands
 def main():
     parser = argparse.ArgumentParser(description="Dicomifier")
     
-    parser.add_argument("--version", action="version", version="%(prog)s 2.4.0")
+    parser.add_argument("--version", action="version", version="%(prog)s 2.5.0")
     
     parser.add_argument(
         "--verbosity", "-v", dest="main_verbosity",
